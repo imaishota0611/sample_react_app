41e05a346d545ea0069c2b4bef93e0ddd80996af
imaishota0611 <102264809+imaishota0611@users.noreply.github.com>
2022-03-24 08:55:28 +0900

    sampleCode_ES2015

--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var1 = "上書き";
+// console.log(var1);
+
+// var var1 = "再宣言";
+// console.log(var1);
+
+// let val2 = "let";
+// console.log(val2)
+
+// val2 = "上書き";
+// console.log(val2)
+
+// let val2 = "再宣言";
+
+// const val3 = "const";
+// console.log(val3);
+
+//val3 = "上書き";
+
+// const val3 = "再宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "shota",
+//   age: 25,
+// };
+// console.log(val4);
+
+// val4.name = "shota2"
+// val4.addres = "tokyo"
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey")
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+//  const name = "shota";
+//  const age = 25;
+
+//従来の方法
+//  const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+//テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+//従来の関数
+//  function func1(str) {
+//    return str;
+//  }
+// console.log(func1("func1です。"));
+
+//アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20))
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "shota",
+//   age: 25,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`
+// console.log(message2);
+
+// const myProfile2 = ["shota", 25];
+// const [name, age] = myProfile2;
+// const message3 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message3);
+
+/**
+ * default値
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにんちは！${name}さん`);
+// sayHello("shota");
